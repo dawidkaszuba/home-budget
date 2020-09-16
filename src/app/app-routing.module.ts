c1f8706b7cc314eb96eb5aededb07dd5a5e4de44
dawidkaszuba <d.kaszuba89@gmail.com>
2020-09-16 23:06:45 +0200

    added AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AuthGuardService } from './auth-guard.service';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  {path: 'mainPage', component: MainPageComponent}
+  {path: 'mainpage', component: MainPageComponent,  canActivate: [AuthGuardService]}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FormsModule } from '@angular/forms';
 import { MainPageComponent } from './main-page/main-page.component';
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
 
 
 
@@ -38,9 +39,10 @@ import { MainPageComponent } from './main-page/main-page.component';
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
-    FormsModule
+    FormsModule,
   ],
-  providers: [JwtClientService],
+  providers: [JwtClientService, { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+    JwtHelperService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
