2ea7f3cbee64ff7562f81d2009622ececec339a2
dawidkaszuba <d.kaszuba89@gmail.com>
2020-09-16 23:12:28 +0200

    added redirection from login to main page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { JwtClientService } from '../security/jwt-client.service';
 import { AuthRequest } from 'src/model/AuthRequest';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -13,7 +14,7 @@ export class LoginComponent implements OnInit {
   username: string;
   password: string;
 
-  constructor(private service: JwtClientService) { }
+  constructor(private service: JwtClientService, private router: Router ) { }
 
   ngOnInit(): void {
   }
@@ -23,5 +24,6 @@ export class LoginComponent implements OnInit {
     const authRequest: AuthRequest = new AuthRequest(this.username, this.password);
     const resp = this.service.generateToken(authRequest);
     resp.subscribe(data => localStorage.setItem('token', data.token));
+    this.router.navigate(['/mainpage']);
   }
 }
