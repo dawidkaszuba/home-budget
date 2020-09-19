e56e59be45121afd2ee87c5bd61e981de7d103da
dawidkaszuba <d.kaszuba89@gmail.com>
2020-09-20 00:13:00 +0200

    set param from to from current month

--- src/app/balance.service.ts
+++ src/app/balance.service.ts
@@ -16,7 +16,10 @@ export class BalanceService {
     const tokenStr = 'Bearer ' + localStorage.getItem('token');
     const userid = localStorage.getItem('userid');
     const headers = new HttpHeaders().set('Authorization', tokenStr);
-    const params = new HttpParams().set('from','2020-08-01').set('to', '2020-08-31');
+    const date = new Date();
+    const firstDayOfCurrentMonth = new Date(date.getFullYear(), date.getMonth(), 1).toISOString().split('T')[0];
+    const lastDayOfCurrentMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).toISOString().split('T')[0];
+    const params = new HttpParams().set('from', firstDayOfCurrentMonth).set('to', lastDayOfCurrentMonth);
     return this.httpService.get<Balance>('http://localhost:8080/users/' + userid + '/balance',
     { headers, responseType: 'json' , params});
   }
