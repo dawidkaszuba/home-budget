1d781e1e6f4386265c1060932322b746491ddcbd
dawidkaszuba <dawidk89@gmail.com>
2022-12-18 22:32:53 +0100

    add mysql dependency

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into categories(name) values("paliwo");
+insert into categories(name) values("żywność");
+
+insert into users(first_name, last_name) values("Dawid", "Kaszuba");
+
+insert into expenses(time, user_id, category_id, value) values('2022-12-12', 1, 1, 100.99);
+insert into expenses(time, user_id, category_id, value) values('2022-12-12', 1, 2, 545.67);
